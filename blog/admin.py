cee4daab855513f113ee9d38ff3f75c1ad02c286
Matt Reece-Jones <MDR824@student.bham.ac.uk>
2020-08-26 21:08:26 +0100

    Model for list Items and associated migration

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Item
 
 admin.site.register(Post)
+admin.site.register(Item)
--- functional_tests.py
+++ functional_tests.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import unittest
+from blog.models import Item
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -61,7 +62,24 @@ class NewVisitorTest(unittest.TestCase):
         self.fail('Finish the test!')
 
 
+class ItemModelTest(TestCase):
 
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
 
 
 if __name__ == '__main__':
